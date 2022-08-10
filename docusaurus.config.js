9a519101ce654d87df73f53a87a6c7886b3598c5
zkan <zkan.cs@gmail.com>
2022-08-10 22:29:49 +0700

    Add slash :bear:

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   title: "The Data Engineering Book",
   // tagline: 'The tagline of my site',
-  url: "https://www.thedataengineeringbook.online",
+  url: "https://www.thedataengineeringbook.online/",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
